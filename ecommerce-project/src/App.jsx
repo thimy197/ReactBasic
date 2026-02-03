9a61508eb8d4e2bedce72ff9f4e964acf90d73c4
thimy197 <daothimy197@gmail.com>
2026-02-03 09:58:00 +0700

    format and group files into the related folders

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router'
-import {Home} from './pages/Home'
-import {Checkout} from './pages/Checkout'
-import {Orders} from './pages/Orders'
-import {Tracking} from './pages/Tracking'
+import {Home} from './pages/home/Home'
+import {Checkout} from './pages/checkout/Checkout'
+import {Orders} from './pages/orders/Orders'
+import {Tracking} from './pages/tracking/Tracking'
 import './App.css'
 
 function App() {
--- ecommerce-project/src/pages/home/Home.jsx
+++ ecommerce-project/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import {Header} from '../Components/Header';
+import {Header} from '../../Components/Header';
 import {Link} from 'react-router';
 import './Home.css';
 
